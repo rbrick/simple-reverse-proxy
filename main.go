ea0bc28e693d92d014f596172f2615040f1d31be
rbrick <rbrickproductions123@gmail.com>
2018-11-01 18:25:13 -0500

    add support for TLS

--- main.go
+++ main.go
@@ -1,11 +1,43 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
 
 func init() {
 
 }
 
 func main() {
-	http.ListenAndServe(":80", NewProxiedHandler("hosts.json"))
+	instance := NewProxiedHandler("hosts.json")
+	go http.ListenAndServe(":80", instance)
+
+	if os.Getenv("SRP_TLS") == "true" {
+		go http.ListenAndServeTLS(":443", os.Getenv("SRP_CERT_FILE"), os.Getenv("SRP_KEY_FILE"), instance)
+	}
+
+	signals := make(chan os.Signal, 1) // allocate a channel with a size of one
+
+	// SIGINT = Ctrl+C
+	// SIGTERM = Termination request
+	// Listen for these
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	var wg sync.WaitGroup
+	{
+		wg.Add(1)
+	}
+
+	go func() {
+		select {
+		case <-signals:
+			wg.Done()
+		}
+	}()
+
+	wg.Wait()
 }
--- types.go
+++ types.go
@@ -14,9 +14,23 @@ type ProxiedHandler struct {
 }
 
 func (p *ProxiedHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	if v, ok := p.ReverseProxies[req.Host]; ok {
+	if v, ok := p.ReverseProxies[req.URL.Host]; ok {
 		v.ServeHTTP(w, req)
+		return
 	}
+
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte(http.StatusText(http.StatusNotFound)))
+}
+
+func clean(s []string) []string {
+	var x []string
+	for _, str := range s {
+		if str != "" {
+			x = append(x, str)
+		}
+	}
+	return x
 }
 
 func NewProxiedHandler(hostFile string) *ProxiedHandler {
@@ -33,7 +47,21 @@ func NewProxiedHandler(hostFile string) *ProxiedHandler {
 	reversedProxies := map[string]*httputil.ReverseProxy{}
 
 	for k, v := range hostRedirectMap {
-		reversedProxies[k] = httputil.NewSingleHostReverseProxy(v)
+		realReverseProxy := httputil.NewSingleHostReverseProxy(v)
+
+		loggingProxy := &httputil.ReverseProxy{
+			Director: func(r *http.Request) {
+				realReverseProxy.Director(r) // run our director first
+				if r.TLS != nil && v.Scheme == "http" {
+					// panic!
+					panic("http over https")
+				} else {
+					log.Printf("sending proxy request on %s://%s%s to %s\n", r.URL.Scheme, k, r.URL.Path, v.Host)
+				}
+			},
+		}
+
+		reversedProxies[k] = loggingProxy
 	}
 
 	return &ProxiedHandler{
